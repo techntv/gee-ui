9d3e481d4734b191491039eb79c0e69d30811ca8
Vinh Nguyen <techntvz@gmail.com>
2025-04-24 09:26:55 +0700

    init gee lib component

---
+++ lib/main.ts
@@ -0,0 +1,2 @@
+export { Button } from './components/Button'
+export { Accordion } from './components/Accordion'
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['lib'],
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  }
+})
