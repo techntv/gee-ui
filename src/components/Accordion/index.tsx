24d56c2425ad70c39b43766b5935cdef13ac2942
Vinh Nguyen <techntvz@gmail.com>
2025-04-24 20:47:53 +0700

    add accordion

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,5 +1,40 @@
 import styles from './styles.module.css'
 
+const data = [
+  {
+    title: 'HTML',
+    content:
+      'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.',
+    key: 'html',
+  },
+  {
+    title: 'CSS',
+    content:
+      'Cascading Style Sheets is a style sheet language used to describe the presentation of a document written in HTML or XML.',
+    key: 'css',
+  },
+  {
+    title: 'JavaScript',
+    content:
+      'JavaScript is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.',
+    key: 'javascript',
+  },
+  {},
+]
+
 export const Accordion = () => {
-    return <div className={styles.accordion}>Accordion</div>
+  return (
+    <div className={styles.accordion}>
+      {data.map((item) => (
+        <div key={item.key}>
+          <h3>{item.title}</h3>
+          <p>{item.content}</p>
+          <span
+            aria-hidden={true}
+            className={styles.accordion_icon}
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
